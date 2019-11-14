cdcc406332cf8681f64be6c7a9a88a790da978b5
cherish <3505162798@qq.com>
2019-11-14 11:56:37 +0800

    修复client1和2切换需要多次授权

--- sso-client2/src/main/java/com/cus/client2/controller/ResourceController.java
+++ sso-client2/src/main/java/com/cus/client2/controller/ResourceController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/client2")
+//@RequestMapping("/client2")
 public class ResourceController {
 
     @GetMapping("/index")
